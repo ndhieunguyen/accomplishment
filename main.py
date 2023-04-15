a2a2c04d653e1a1294e0104de4df53cfe24809aa
ndhieunguyen <hieunguyen.kientuong@gmail.com>
2023-04-15 16:27:10 +0700

    feat: improve filter

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ st.set_page_config(
 )
 
 ### SIDEBAR
-st.sidebar.markdown("<h1 style='text-align: center;'>Filter</h1>", unsafe_allow_html=True)
-name = st.sidebar.text_input("Name", "", help="The phrase contained in the name of the accomplishment")
+st.sidebar.markdown("<h1 style='text-align: center;'>Tools</h1>", unsafe_allow_html=True)
+name = st.sidebar.text_input("Name", "", help="The phrase contained in the name of the accomplishment").lower().strip()
 organizations = [
     "Tuyen Thanh Primary school",
     "Vo Duy Duong secondary school",
@@ -29,6 +29,12 @@ organization = st.sidebar.multiselect(
 from_year = st.sidebar.date_input("From date", value=datetime.date(2002, 8, 25), help="The lower bound of date range")
 to_year = st.sidebar.date_input("To date", help="The upper bound of date range")
 
+sorted_by = st.sidebar.multiselect(
+    "Sorted by",
+    ["Name", "Organization", "Date"],
+    help="The columns by which the data frame is sorted",
+)
+
 filter_button = st.sidebar.button("OK")
 
 
@@ -40,7 +46,7 @@ st.text("Nguyen Doan Hieu Nguyen - ndhieunguyen")
 
 @st.cache_data
 def load_data():
-    return pd.read_csv("accomplishment.csv", index_col=0)
+    return pd.read_csv("accomplishment.csv", index_col=0).reset_index(drop=True)
 
 
 data = load_data()
@@ -50,11 +56,12 @@ data = data.drop(["Link"], axis=1)
 
 
 if filter_button:
-    data = data[data["Name"].str.contains(name)]
+    data = data[data["Name"].str.lower().str.contains(name)]
     if len(organization) > 0:
         for org in organization:
             data = data[data["Organization"].str.contains(org)]
     # st.dataframe(data=data, use_container_width=True)
+    data = data.sort_values(by=sorted_by)
     data = data.to_html(escape=False)
     st.write(data, unsafe_allow_html=True)
 else:
@@ -69,3 +76,11 @@ st.download_button(
     file_name="ndhieunguyen_accomplishment.csv",
     mime="text/csv",
 )
+
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden; }
+        footer {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
