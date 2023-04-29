c776e52be00fc98577a3d47b1be6d9dd83fb4a8d
ndhieunguyen <hieunguyen.kientuong@gmail.com>
2023-04-29 10:32:23 +0700

    feat: multi pages

--- pages/1_🏆_Accomplishments.py
+++ pages/1_🏆_Accomplishments.py
@@ -12,7 +12,15 @@ st.set_page_config(
 
 ### SIDEBAR
 st.sidebar.markdown("<h1 style='text-align: center;'>Tools</h1>", unsafe_allow_html=True)
-name = st.sidebar.text_input("Name", "", help="The phrase contained in the name of the accomplishment").lower().strip()
+name = (
+    st.sidebar.text_input(
+        "Name",
+        "",
+        help="The phrase contained in the name of the accomplishment",
+    )
+    .lower()
+    .strip()
+)
 organizations = [
     "Tuyen Thanh Primary school",
     "Vo Duy Duong secondary school",
@@ -27,8 +35,19 @@ organization = st.sidebar.multiselect(
     help="The organization that recognized the accomplishment",
 )
 
-from_year = st.sidebar.date_input("From date", value=datetime.date(2002, 8, 25), help="The lower bound of date range")
-to_year = st.sidebar.date_input("To date", help="The upper bound of date range")
+from_date = st.sidebar.date_input(
+    "From date",
+    value=datetime.date(2002, 8, 25),
+    min_value=datetime.date(2002, 8, 25),
+    max_value=datetime.date.today(),
+    help="The lower bound of date range",
+)
+
+to_date = st.sidebar.date_input(
+    "To date",
+    value=None,
+    help="The upper bound of date range",
+)
 
 sorted_by = st.sidebar.multiselect(
     "Sorted by",
@@ -38,49 +57,50 @@ sorted_by = st.sidebar.multiselect(
 
 filter_button = st.sidebar.button("OK")
 
-st.markdown(
-    """
-    <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
-    <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="ndhieunguyen" data-version="v1">
-        <a class="badge-base__link LI-simple-link" href="https://linkedin.com/in/ndhieunguyen?trk=profile-badge">Nguyen Doan Hieu Nguyen</a>
-    </div>
-    """,
-    unsafe_allow_html=True,
-)
-
 
 ### MAIN
-# st.header("Accomplishment")
 st.markdown("<h1 style='text-align: center;'>Accomplishment</h1>", unsafe_allow_html=True)
-st.text("Nguyen Doan Hieu Nguyen - ndhieunguyen")
 
 
 @st.cache_data
 def load_data():
-    return pd.read_csv("accomplishment.csv", index_col=0).reset_index(drop=True)
+    data_frame = pd.read_csv("accomplishment.csv", index_col=0).reset_index(drop=True)
+    data_frame["Date"] = pd.to_datetime(data_frame["Date"], format="%B %Y").dt.date
+    return data_frame
 
 
 data = load_data()
-# data["View"] = data["View"].apply(make_clickable)
-data["Name"] = data.apply(lambda x: create_clickable_link(x["Name"], x["Link"]), axis=1)
-data = data.drop(["Link"], axis=1)
 
 
 if filter_button:
+    # filter by name
     data = data[data["Name"].str.lower().str.contains(name)]
+
+    # filter by organization
     if len(organization) > 0:
-        for org in organization:
-            data = data[data["Organization"].str.contains(org)]
-    # st.dataframe(data=data, use_container_width=True)
+        data = data[data["Organization"].isin(organization)]
+
+    # filter by date
+    if len(data) > 0 and from_date and to_date:
+        data = data[(from_date <= data["Date"]) & (data["Date"] <= to_date)]
+
+    # sort by
     data = data.sort_values(by=sorted_by)
+
+    # create clickable name
+    if len(data) > 0:
+        data["Name"] = data.apply(lambda x: create_clickable_link(x["Name"], x["Link"]), axis=1)
+    data = data.drop(["Link"], axis=1)
+
+    # display data frame
     st.write(data.to_html(escape=False), unsafe_allow_html=True)
 else:
-    # st.dataframe(data=data, use_container_width=True)
+    data["Name"] = data.apply(lambda x: create_clickable_link(x["Name"], x["Link"]), axis=1)
+    data = data.drop(["Link"], axis=1)
     st.write(data.to_html(escape=False), unsafe_allow_html=True)
 
 st.download_button(
     label="Download accomplishments as CSV",
-    # data=data.to_csv().encode("utf-8"),
     data=data.to_csv().encode("utf-8"),
     file_name="ndhieunguyen_accomplishment.csv",
     mime="text/csv",
---
+++ pages/2_💫_Memories.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden; }
+        footer {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
