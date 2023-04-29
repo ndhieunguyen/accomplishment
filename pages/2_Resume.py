89dc0bd0756966e4eaefc18058023fcb115f2942
ndhieunguyen <hieunguyen.kientuong@gmail.com>
2023-04-29 19:42:16 +0700

    feat: change theme

--- pages/2_Resume.py
+++ pages/2_Resume.py
@@ -1,20 +1,19 @@
 import streamlit as st
-import base64
+import os
 from utils import show_pdf
 
-file_path = "https://github.com/ndhieunguyen/accomplishment/blob/main/images/Nguyen_Doan_Hieu_Nguyen___AI_engineer___Resume.pdf"
+file_path = os.path.join("images", "Nguyen_Doan_Hieu_Nguyen___AI_engineer___Resume.pdf")
 show_pdf(file_path)
 
 with open(file_path, "rb") as pdf_file:
     PDFbyte = pdf_file.read()
 
-st.download_button(
+st.sidebar.download_button(
     label="Download Resume",
     data=PDFbyte,
     file_name="Nguyen_Doan_Hieu_Nguyen___AI_engineer___Resume.pdf",
     mime="application/octet-stream",
 )
-
 
 hide_menu_style = """
         <style>
